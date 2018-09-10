7f4f94b365fb091265262e652b213df172f2feee
Jonathan Tung <jtung23@gmail.com>
2018-09-10 07:30:22 -0700

    extracts from each page

--- bikeScraper.js
+++ bikeScraper.js
@@ -1,14 +1,28 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
-request('https://sfbay.craigslist.org/search/bia?query=cyclocross', function(err, res, html) {
-    if (!err && res.statusCode == 200) {
-        const $ = cheerio.load(html)
-        let arr = []
-        $('.result-row').each((i, val) =>{
-            newString = $(val).text().replace(/\s+/g,' ').trim();
-            arr.push(newString)
-        })
-        console.log(arr[0])
-    }
-})
+let scrape = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://sfbay.craigslist.org/search/bia?query=cyclocross');
+        // Scrape
+    await page.click('li.result-row:nth-child(1) > a:nth-child(1)');
+    await page.waitFor(1000);
+    const result = await page.evaluate( () => {
+        let title = document.querySelector('#titletextonly').innerText
+        let price = document.querySelector('.price').innerText
+        let postBody = document.querySelector('#postingbody').innerText
+        let attrGroup = document.querySelector('p.attrgroup').innerHTML
+        return {
+            title,
+            price,
+            postBody,
+            attrGroup
+        }
+    })
+    browser.close();
+    return result;
+}
+
+scrape().then((value) => {
+    console.log(value); // Success!
+});

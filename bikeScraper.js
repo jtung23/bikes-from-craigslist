cf8cd18eb3e77c69c948fa4c1b2c6ff186bc24c1
Jonathan Tung <jtung23@gmail.com>
2018-09-11 17:33:42 -0700

    addtl changes

--- bikeScraper.js
+++ bikeScraper.js
@@ -3,9 +3,8 @@ const testStrings = require('./testStrings.js');
 
 const query = ['cyclocross']
 const maxPrice= 1000
-const minFrameSize = 60 // in cm
+const minFrameSize = 60 // in cm, 
 
-let postTest = "'I built up each part of this bike including the wheels. When I have time to ride I always go for the full suspension so this bike never gets ridden and it deserves to. It is in good working order!\n\n text or call 3one0 seven two 1 five 7 two 8\n -Bill\n\n Steel Rock Lobster frame 56 Cm\n Sram Rival 10 speed shifter\n Praxis 10 speed cassette 40/11\n Sram Red carbon crank arms 172.5 mm\n Praxis chain ring 40 tooth\n Sram X7 MTB rear derailuer\n Shimano XT spd pedals\n Shimano 105 hubs\n WTB ChrisCross wheels\n WTB Nano 40 front tire Tubeless\n WTB ChrisCross back tire Tubeless\n Bontrager RXL 7 degree 100 mm stem\n Bontrager Race aluminum bars 46 cm\n Bontrager RL carbon fork\n Bontrager Montrose elite hollow titanium seat\n Paul cantilever brakes!\n Gravity dropper 3 position seat post\n I do not recall the bottom bracket or headset make but they are quality and in good working order\n\n\n\n\n',"
 let scrape = async (val) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -54,19 +53,6 @@ let scrape = async (val) => {
     }
     console.log(links)
     console.log(links[0])
-
-        //     if (priceChecker(price)) { // if meets price condition then continue
-                
-        //         return {
-        //             title,
-        //             price,
-        //             postBody,
-        //             attrGroup
-        //         }          
-        //     }
-
-        // })
-    // })
     browser.close();
 }
 
@@ -93,18 +79,8 @@ const attrChecker = (attrGroup) => { //checks attrgroup & returns true if meets 
     let iso = frameSection.indexOf('<') // gets index of <b> tag
     let num = frameSection.slice(0, iso) // isolates the size only
     
-    // determine if medium. M, 35cm
-    const sizes = {
-        s: ['small', 'sm', 's'],
-        m: ['medium', 'med', 'm'],
-        l: ['large', 'lg', 'l'],
-        xl: ['extra large', 'xl'],
-        xxl: 'xxl',
-        xxxl: 'xxxl'
-    }
-// parseInt of a str is falsey
+    // parseInt of a str is falsey, NaN
     if (parseInt(num[0])) {
-        console.log('detects is num')
         // if first value is a number then proceed as e.g. 60cm
         let int = parseInt(num.slice(0,2))
         if (int < minFrameSize) {
